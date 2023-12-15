c22bc0d727b085ca8bc4896bca4778ef4de795fe
tung-ha125 <96463130+tung-ha125@users.noreply.github.com>
2023-12-16 00:31:18 +0700

    Update transaction employee

--- Backend/Transaction/urls.py
+++ Backend/Transaction/urls.py
@@ -5,6 +5,10 @@ urlpatterns = [
     path('create_shipment', create_shipment),
     path('transaction_employee/shipment_to_consolidation', create_transaction_to_correspond_consolidation_point),
     path('transaction_employee/shipment_from_consolidation', confirm_shipment_from_correspond_consolidation_department),
+    path('transaction_employee/create_transaction_to_receiver', create_transaction_to_receiver),
+    path('transaction_employee/confirm_complete_shipment', confirm_complete_shipment),
+    path('transaction_employee/confirm_failed_shipment_and_send_back', confirm_failed_shipment_and_send_back),
+    path('transaction_employee/list_complete_and_fail_shipment', list_complete_and_fail_shipment),
 
     path('consolidation_employee/shipment_from_transaction', confirm_transaction_from_correspond_transaction_department),
     path('consolidation_employee/shipment_to_consolidation', create_transaction_to_target_consolidation_point),
