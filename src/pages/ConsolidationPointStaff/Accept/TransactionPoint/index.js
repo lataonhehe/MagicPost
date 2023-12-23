2d0e0b2faf396667332b8bfe6cf2693da57a1216
tthanh25 <21020790@vnu.edu.vn>
2023-12-23 23:43:25 +0700

    fix tiep :>

--- src/pages/ConsolidationPointStaff/Accept/TransactionPoint/index.js
+++ src/pages/ConsolidationPointStaff/Accept/TransactionPoint/index.js
@@ -179,7 +179,7 @@ EnhancedTableHead.propTypes = {
 };
 
 function EnhancedTableToolbar(props) {
-  const { numSelected, handleDelete } = props;
+  const { numSelected, handleAccept } = props;
   return (
     <Toolbar
       sx={{
@@ -213,7 +213,7 @@ function EnhancedTableToolbar(props) {
           component="div"
           fontWeight="Bold"
         >
-          Quản lí nhân viên
+          Xác nhận đơn hàng
         </Typography>
       )}
 
@@ -223,9 +223,9 @@ function EnhancedTableToolbar(props) {
             sx={{ fontSize: "14px", marginRight: "16px" }}
             variant="contained"
             startIcon={<DeleteIcon sx={{ fontSize: "24px" }} />}
-            onClick={handleDelete}
+            onClick={handleAccept}
           >
-            Xóa
+            Xác nhận
           </Button>
         </Tooltip>
       ) : (
@@ -239,168 +239,6 @@ EnhancedTableToolbar.propTypes = {
   numSelected: PropTypes.number.isRequired,
   handleDelete: PropTypes.func.isRequired,
 };
-
-export function InputAdornments({ fetchData }) {
-  const [showPassword, setShowPassword] = useState(false);
-  const [values, setValues] = useState({
-    username: "",
-    password: "",
-    confirmPassword: "",
-  });
-  const [usernameError, setUsernameError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
-  const [confirmpwError, setConfirmPWError] = useState("");
-
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
-
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault();
-  };
-
-  const handleChange = (field, value) => {
-    setValues((prevValues) => ({
-      ...prevValues,
-      [field]: value,
-    }));
-  };
-
-  const handleAddUser = async () => {
-    try {
-      const token = localStorage.getItem("Token");
-      const addResponse = await fetch(
-        "http://127.0.0.1:8000/Account/register",
-        {
-          method: "POST",
-          headers: {
-            Authorization: `Token ${token}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            username: values.username,
-            password: values.password,
-          }),
-        }
-      );
-      fetchData();
-      if (!addResponse.ok) {
-        setUsernameError("Username không hợp lệ!");
-        throw new Error("Add user request failed");
-      }
-    } catch (error) {}
-  };
-
-  const onButtonClick = () => {
-    setUsernameError("");
-    setPasswordError("");
-    setConfirmPWError("");
-    if (values.username.trim() === "") {
-      setUsernameError("Hãy nhập username");
-      return;
-    }
-
-    if (!/^[a-zA-Z0-9_-]+$/.test(values.username.trim())) {
-      setUsernameError("Username không hợp lệ!");
-      return;
-    }
-
-    if (values.password.trim() === "") {
-      setPasswordError("Hãy nhập mật khẩu!");
-      return;
-    }
-    if (values.confirmPassword.trim() === "") {
-      setConfirmPWError("Hãy nhập lại mật khẩu!");
-      return;
-    }
-    if (values.confirmPassword !== values.password) {
-      setConfirmPWError("Mật khẩu không khớp!");
-      return;
-    }
-
-    if (values.password.length < 5) {
-      setPasswordError("Mật khẩu phải từ 5 kí tự trở lên!");
-      return;
-    }
-
-    handleAddUser();
-  };
-
-  return (
-    <Box sx={{ margin: "auto" }}>
-      <div>
-        <FormControl
-          sx={{ m: 1, width: "45ch", fontSize: "32px" }}
-          variant="outlined"
-        >
-          <TextField
-            sx={{ m: 1, width: "25ch" }}
-            id="outlined-adornment-weight"
-            placeholder="Nhập username"
-            value={values.username}
-            onChange={(e) => handleChange("username", e.target.value)}
-            inputProps={{
-              "aria-label": "weight",
-            }}
-          />
-          <label className={cx("errorLabel")}>{usernameError}</label>
-          <TextField
-            sx={{ m: 1, width: "25ch" }}
-            id="outlined-adornment-password"
-            type={showPassword ? "text" : "password"}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            placeholder="Nhập mật khẩu:"
-            value={values.password}
-            onChange={(e) => handleChange("password", e.target.value)}
-          />
-          <label className={cx("errorLabel")}>{passwordError}</label>
-          <TextField
-            sx={{ m: 1, width: "25ch" }}
-            id="outlined-adornment-password"
-            type={showPassword ? "text" : "password"}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            placeholder="Nhập lại mật khẩu:"
-            value={values.confirmPassword}
-            onChange={(e) => handleChange("confirmPassword", e.target.value)}
-          />
-          <label className={cx("errorLabel")}>{confirmpwError}</label>
-          <ColorButton
-            variant="contained"
-            onClick={onButtonClick}
-            sx={{
-              fontSize: "16px",
-              margin: "32px",
-              marginLeft: "132px",
-              width: "160px",
-            }}
-          >
-            Xác nhận
-          </ColorButton>
-        </FormControl>
-      </div>
-    </Box>
-  );
-}
 
 export default function ConsolStaffAcceptConsolidation() {
   const [addUser, setAddUser] = useState(false);
@@ -446,7 +284,7 @@ export default function ConsolStaffAcceptConsolidation() {
     } catch (error) {}
   };
 
-  const handleDelete = async () => {
+  const handleAccept = async () => {
     try {
       const token = localStorage.getItem("Token");
 
@@ -470,11 +308,6 @@ export default function ConsolStaffAcceptConsolidation() {
       setSelected([]);
     } catch (error) {}
   };
-
-  function handleAddUser() {
-    fetchData();
-    setAddUser(!addUser);
-  }
 
   useEffect(() => {
     // Retrieve employee list from local storage when component mounts
@@ -549,21 +382,10 @@ export default function ConsolStaffAcceptConsolidation() {
   return (
     <>
       <Box sx={{ width: "94%", margin: "auto" }}>
-        <ColorButton
-          startIcon={<PersonAddIcon />}
-          variant="contained"
-          onClick={handleAddUser}
-          sx={{ fontSize: "18px" }}
-        >
-          Thêm nhân viên{" "}
-        </ColorButton>
-        <Collapse in={addUser} timeout="auto">
-          {addUser && <InputAdornments fetchData={fetchData} />}
-        </Collapse>
-        <Paper sx={{ width: "100%", mb: 2 }} elevation={3}>
+        <Paper sx={{ width: "100%", mb: 2, marginTop: "48px" }} elevation={3}>
           <EnhancedTableToolbar
             numSelected={selected.length}
-            handleDelete={handleDelete}
+            handleAccept={handleAccept}
           />
           <Divider />
           <TableContainer>
